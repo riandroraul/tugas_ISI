c3a8b6e0c21395364754468704b967aaadf6bff0
riandroraul <riandroraul@gmail.com>
2022-06-17 11:24:15 +0700

    first commit

--- test2.js
+++ test2.js
@@ -1,49 +1,51 @@
-function tableDynamic(arraySatu, val) {
-    const hasilMax = Math.max.apply(null, arraySatu); 
-    const panjang = `${hasilMax}`.length
-    const panjangArray = arraySatu.length; 
-    const akhirTable = Math.floor(panjangArray / val); 
-    let a = "+";
-    let f = val; 
-    for (let i = 0; i < val; i++) {
-        for (let s = 0; s < panjang; s++) {
-             a += "="; 
-        } 
-        a += "+";
+function table(arr, max) {
+    let nilaiMax = Math.max.apply(undefined, arr); // nilai max dari tiap2 elemen arr (number)
+    let longEl = nilaiMax.toString().length; // elemen yang paling panjang pada arr (string)
+    let lengthArray = arr.length; // panjang array
+    // const lastTable = Math.floor(lengthArray / max); 
+    let teks = "+";
+    for (let i = 0; i < max; i++) {
+        for (let j = 0; j < longEl; j++) {
+             teks += "="; 
+        }
+        teks += "+";
     }
 
-    a += "\n"
-    arraySatu.forEach(function (x, y) {
-        a += "|"
-        for (let l = 0; l < panjang - `${x}`.length; l++) {
-            a += "*";
+    teks += "\n"
+    arr.forEach( function(el, index) {
+        // el => tiap2 elemen pada array (number)
+        teks += "|"
+        for (let i = 0; i < longEl - el.toString().length; i++) {
+            teks += "*";
         }
-        a += `${x}`;
-        if ((y + 1) % val == 0) {
-            a += "|"
-            a += "\n";
-            for (let i = 0; i < f; i++) {
-                a += "+";
-                for (let j = 0; j < panjang; j++) {
-                    a += "=";
+        teks += el;
+        if ((index + 1) % max == 0) { // jika sisa bagi index dengan max == 0
+            teks += "|";
+            teks += "\n";
+            for (let i = 0; i < max; i++) {
+                teks += "+";
+                for (let j = 0; j < longEl; j++) {
+                    teks += "=";
                 }
             }
-            a += "+";
-            a += "\n";
+            teks += "+";
+            teks += "\n";
         }
     });
-    a += "|"                                                          
-    a += "\n"                                                         
-    for (let i = 0; i < panjangArray % val; i++) {
-        a += "+";
-        for (let s = 0; s < panjang; s++) {
-            a += "=";
+    teks += "|"; 
+    teks += "\n";
+    // console.log(lengthArray % max)
+    for (let i = 0; i < lengthArray % max; i++) {
+        teks += "+";
+        for (let j = 0; j < longEl; j++) {
+            teks += "=";
         }
     }
-    a += "+";
-    return a;
+    teks += "+";
+    return teks;
+    // console.log(nilaiMax)
+    // return lengthArray
 }
 
 
-console.log(tableDynamic([12, 79996, 7655, 777, 876, 8866, 97, 788
-    , 89, 9, 888839, 8882, 88, 234, 234,4,23434,23423,3453234,234234,2342], 8));
+console.log(table([1090092, 79996, 7655, 777, 876, 8866, 97, 788, 89, 9, 888839, 8882, 88, 234, 234, 4, 23434, 23423, 3900453223, 3900453239, 3900453299, 234234, 2342], 6));
