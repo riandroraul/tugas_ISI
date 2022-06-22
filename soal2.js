5198ce8817fa68d18aa864058685de793c561b98
riandroraul <riandroraul@gmail.com>
2022-06-22 15:39:10 +0700

    soal 2

--- soal2.js
+++ soal2.js
@@ -18,24 +18,28 @@ function hillOrValley(arr){
         // [3, -1, -5, -5, 2,4,7,5,1,1,1,4]
         if(i == 0){
             if(arr[i] > arr[i+1]){      // jika index saat ini lebih besar dari index selanjutnya 
-                count++;                // count + 1
-                // console.log(count, i)
+                count++;                       // count + 1
+                console.log(count, i) // index ke 0 count
             }
         }
-        else if(arr[i] == arr[arr.length - 1]){ // jika nilai array index ke i sama dengan nilai array index terakhir
+        else if(arr[i] == arr[arr.length - 1]){     // jika nilai array index ke i sama dengan nilai array index terakhir
             if(arr[i] > arr[i-1]){
                 count++;
-                // console.log(count, i)
+                console.log(count, i)       //index ke 5 dan ke 11 count
             }
         }
         else{
-            if(arr[i] > arr[i+1] && arr[i] < arr[i-1]){
+            if(arr[i] == arr[i+1] && arr[i] < arr[i-1]){
                 count++;
-                // console.log(count, i)
+                console.log(count, i) // index ke 2 dan ke 8 count
             }
+            // else if(arr[i] < arr[i+1] && arr[i] > arr[i-1]){
+            //     count++;
+            //     console.log(count, i)
+            // }
         }
     }
     return count;
 }
 
-console.log(hillOrValley([3, -1, -5, -5, 2, 4,7,5,1,1,1,4]));
+console.log(hillOrValley([3,-1,-5,-5,2,4,7,5,1,1,1,4]));
